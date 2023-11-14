42f1e51dc97b4dd8ada1f404ea7bd760ac6e64f0
Zuzanna <zuzanna.tarka@signavio.com>
2023-11-14 14:07:41 +0100

    use endpoint as resolver

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ import { ApolloServer } from "@apollo/server"
 import { startStandaloneServer } from '@apollo/server/standalone'
 
 const typeDefs = `
+
+ type CityAddress {
+    city: String
+    state: String
+    country_code: String
+ }
+
   type City {
     name: String
     country: String
+    address: CityAddress
   }
 
   type Query {
@@ -12,15 +20,21 @@ const typeDefs = `
   }
 `
 
-const resolvers = {
-    Query: {
-        city: (_, args) => city[args.cityName]
-    }
+const fetchCity = async url => {
+  const result = await fetch(url)
+  const response = await result.json()
+  return response[0]
 }
 
-const city = {
-    Warsaw: {name: 'Warsaw'},
-    Berlin: {name: 'Berlin'}
+const resolvers = {
+    Query: {
+        city: async (_, args) => {
+          const url = `https://nominatim.openstreetmap.org/search?addressdetails=1&q=${args.cityName}&format=jsonv2&limit=1`
+          const result = await fetchCity(url)
+          console.log('result', result)
+          return {name: result.name, country: result.address.country, address: result.address}
+        }
+    }
 }
 
 const server = new ApolloServer({typeDefs, resolvers})
