8fede591829c593263d1058146d250496936dc65
Zuzanna <zuzanna.tarka@signavio.com>
2023-11-17 13:22:38 +0100

    add type resolver

--- index.js
+++ index.js
@@ -9,9 +9,15 @@ const typeDefs = `
     country_code: String
  }
 
+ type Temperature {
+  temperature: Float
+  unit: String
+ }
+
   type City {
     name: String
     country: String
+    temperature: Temperature
     address: CityAddress
   }
 
@@ -31,9 +37,22 @@ const resolvers = {
         city: async (_, args) => {
           const url = `https://nominatim.openstreetmap.org/search?addressdetails=1&q=${args.cityName}&format=jsonv2&limit=1`
           const result = await fetchCity(url)
-          console.log('result', result)
-          return {name: result.name, country: result.address.country, address: result.address}
+          return {
+            name: result.name, 
+            country: result.address.country, 
+            address: result.address,
+            lat: result.lat,
+            lon: result.lon
+          }
         }
+    },
+    City: {
+      temperature: async parent => {
+        const url = `https://api.open-meteo.com/v1/forecast?latitude=${parent.lat}&longitude=${parent.lon}&current=temperature_2m`
+        const resp = await fetch(url).then(response => response.json())
+        console.log(resp)
+        return {temperature: resp.current.temperature_2m, unit: resp.current_units.temperature_2m}
+      }
     }
 }
 
